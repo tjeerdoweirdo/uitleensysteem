3c57faac91b803e1f58959d64eafe5f351d548aa
tjeerdoweirdo <146167102+tjeerdoweirdo@users.noreply.github.com>
2023-12-11 15:31:24 +0100

    e

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Item Lending App</title>
+</head>
+<body>
+
+    <header>
+        <h1>Item Lending App</h1>
+    </header>
+
+    <main>
+        <section class="hero">
+            <h2>Share. Borrow. Connect.</h2>
+            <p>Join our community and lend or borrow items easily.</p>
+            <a href="login.php" class="btn">Get Started</a>
+        </section>
+
+        <section class="features">
+            <div class="feature">
+                <h3>Easy Lending</h3>
+                <p>Lend your items to others with just a few clicks.</p>
+            </div>
+            <div class="feature">
+                <h3>Quick Borrowing</h3>
+                <p>Browse available items and borrow them instantly.</p>
+            </div>
+            <div class="feature">
+                <h3>Secure Transactions</h3>
+                <p>Our platform ensures secure and trustworthy transactions.</p>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 Item Lending App</p>
+    </footer>
+
+</body>
+</html>
