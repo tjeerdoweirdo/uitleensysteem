0b439af644dfc53f484623bcc292d03aed472720
finnhet <finnhettinga@gmail.com>
2023-12-19 09:15:57 +0100

    e

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <!-- Add your additional styles or scripts here -->
 </head>
 <body>
 
@@ -18,6 +16,8 @@
                 <li class="nav-item"><a class="nav-link text-white" href="admindashboard.php">Dashboard</a></li>
                 <li class="nav-item"><a class="nav-link text-white" href="add_item.php">Add Item</a></li>
                 <li class="nav-item"><a class="nav-link text-white" href="gebruikertvg.php">Gebruikertvg</a></li>
+                <li class="nav-item"><a class="nav-link text-white" href="logout.php">log uit</a></li>
+            </ul>
             </ul>
         </nav>
     </div>
--- paginas/gebruikertvg.php
+++ paginas/gebruikertvg.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include '../includes/header.php';
 
 if (!isset($_SESSION['user_id']) ) {
     header('Location: login.php');
