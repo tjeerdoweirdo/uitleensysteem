93147d202d213ac602b77ade9ba495b164c1a46c
Zombisaster <zombisaster@gmail.com>
2023-12-19 11:00:56 +0100

    Removed CSS
    copypasted style into document

--- paginas/categorieen.php
+++ paginas/categorieen.php
@@ -1,10 +1,11 @@
+<!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elektronica Uitleen Categorieën</title>
-    <link rel="stylesheet" href="cat.css">
+    <link href="cat.css" rel="stylesheet">
 </head>
 
 <body>
@@ -13,7 +14,7 @@
     </header>
 
     <main>
-        <section class="category">
+        <section class="category"> 
             <div class="category-box">
                 <h3>Laptops</h3>
                 <img src="..." class="category-box-img" alt="Product Photo Laptops">
@@ -38,3 +39,38 @@
     </main>
 </body>
 
+
+<!-- CSS -->
+<style>
+/* Algemene Document */
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+/* Categorie Sectie */
+.category {
+    width: 100%;
+    padding: 60px 0px;
+    background-color: #adb5bd;
+    display: flex;
+    justify-content: center;
+    column-gap: 20px;
+}
+
+.category-box {
+    width: 160px;
+    height: 160px;
+    background-color: #adb5bd;
+}
+
+.category-box h3 {
+    font-size: 26px;
+    line-height: 32px;
+    color: #000;
+    font-family: Arial, sans-serif;
+    font-weight: 600;
+    text-transform: capitalize;
+}
+</style>
