575c58ab11f76cebbcce973896fe3cfe49862cc2
Zombisaster <zombisaster@gmail.com>
2023-12-19 10:51:19 +0100

    begin categorieën pagina
    css + html

---
+++ paginas/categorieen.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elektronica Uitleen Categorieën</title>
+    <link rel="stylesheet" href="cat.css">
+</head>
+
+<body>
+    <header>
+        <h3>Categorieën</h3>
+    </header>
+
+    <main>
+        <section class="category">
+            <div class="category-box">
+                <h3>Laptops</h3>
+                <img src="..." class="category-box-img" alt="Product Photo Laptops">
+            </div>
+            <div class="category-box">
+                <h3>Monitoren</h3>
+                <img src="..." class="category-box-img" alt="Product Photo Monitoren">
+            </div>
+            <div class="category-box">
+                <h3>Muizen</h3>
+                <img src="..." class="category-box-img" alt="Product Photo Muizen">
+            </div>
+            <div class="category-box">
+                <h3>Toetsenborden</h3>
+                <img src="..." class="category-box-img" alt="Product Photo Toetsenborden">
+            </div>
+            <div class="category-box">
+                <h3>Camera's</h3>
+                <img src="..." class="category-box-img" alt="Product Photo Cameras">
+            </div>
+        </section>
+    </main>
+</body>
+
