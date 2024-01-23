f7fca96cfd43acc8b82445a96fc30bb52de7233c
farid422 <farid.schreuder@hotmail.com>
2024-01-23 10:36:01 +0100

    .

--- includes/log.inc.php
+++ includes/log.inc.php
@@ -2,15 +2,20 @@
 
 require_once 'db_connection.php';
 
-$sql = "SELECT items *, GROUP_CONCAT(log.logEntry ORDER BY log.logDate DESC) AS itemLogs
-        FROM items
-        LEFT JOIN log ON items.itemId = log.itemId
-        GROUP BY items.itemId;";
+$sql = "SELECT items.itemId, GROUP_CONCAT(log.logEntry ORDER BY log.logDate DESC) AS itemLogs
+FROM items
+LEFT JOIN log ON items.itemId = log.itemId
+GROUP BY items.itemId;";
         
 $result = mysqli_query($conn, $sql);
 
-if (isset($_POST)['addlog']){
+if (isset($_POST['addlog'])){
+        $log_id = $_POST['log_id'];
+        $logEntry = $_POST['addlog'];
+        // $itemId = $_POST[''];
+    
+        $sqladdlog = "INSERT INTO log (logEntry) VALUES ('$logEntry')";
+        $resultaddlog = mysqli_query($conn, $sqladdlog);
 
-        $sqladdlog = 
-        $resultaddlog = mysqli_query($conn, $sqladdlog)
-}
+        if($resultaddlog){header("Location:../popuplog.php");}
+    }
