4807bc4cf185f2a0935bda0239ec673b319fa9c7
tinnit0 <leusinkthomas@gmail.com>
2024-01-23 09:39:54 +0100

    products heeft nu 2 versies

--- js/prod.js
+++ js/prod.js
@@ -10,3 +10,44 @@ function openEditModal(itemId) {
         console.error('Error: Unable to retrieve product information for itemId ' + itemId);
     }
 }
+$(document).ready(function () {
+    $('.meer').on('click', function () {
+        $('#modal1').modal('show');
+
+        var $tr = $(this).closest('tr');
+
+        var itemId = $tr.data('item-itemid');
+        var itemName = $tr.data('item-name');
+        var itemDescription = $tr.data('item-description');
+        var itemNumber = $tr.data('item-number');
+        var itemDout = $tr.data('item-dout');
+        var itemDin = $tr.data('item-din');
+        var itemPicture = $tr.data('item-picture');
+        var itemState = $tr.data('item-state');
+
+        $('#modal1Label').text(itemName);
+        $('#update_id').val(itemId);
+        $('#item').val(itemName);
+        $('#omschrijving').val(itemDescription);
+        $('#nummer').val(itemNumber);
+        $('#datumuit').val(itemDout);
+        $('#datumin').val(itemDin);
+        $('#staat').val(itemState);
+        $('#foto').val(itemPicture);
+
+
+    });
+});
+$(document).ready(function () {
+    $('.verwijder').on('click', function () {
+        $('#verwijdermodal').modal('show');
+
+        console.log('Delete ID:', $('#delete_id').val());
+
+        var $tr = $(this).closest('tr');
+        var itemId = $tr.data('itemItemid');
+
+
+        $('#delete_id').val(itemId);
+    });
+});
